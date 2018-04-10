63e197a4640087048b7647ffea11ebe44f031d20
Marc Nieto <marc@itsunmute.com>
2018-04-10 01:26:42 -0700

    Initial Commit

---
+++ ReadIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReadIt
+//
+//  Created by Marc Nieto on 4/10/18.
+//  Copyright © 2018 Marc Nieto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
